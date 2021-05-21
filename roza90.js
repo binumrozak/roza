204f4db7198ba66bf878d8f9f9d982ebbed2f80b
binumrozak <84512591+binumrozak@users.noreply.github.com>
2021-05-21 16:54:24 +0800

    Delete roza90.js

--- roza90.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-
-  'https://www.achievablecpmrevenue.com/junfea7n?key=56bb7f203a06c32eece56e5f1e6eff75'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
